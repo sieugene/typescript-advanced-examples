f79d75d7ad9485373e35746d8f04b82b35545c23
sieugene <sieugene@mail.ru>
2021-02-13 15:35:07 +0300

    homework 2

--- homework/2lesson/source.ts
+++ homework/2lesson/source.ts
@@ -1,79 +1,110 @@
-var emitter = {};
+interface Emitter {
+  on: (type: TypeMain, handler: HandlerType) => Emitter;
+  off: (type: TypeMain, handler: HandlerType) => Emitter | undefined;
+  trigger: (event: EventType, args: ArgsType) => Emitter | undefined;
+  _dispatch: (event: EventType, args: ArgsType) => Emitter | undefined;
+  _offByHandler: (type: TypeMain, handler: HandlerType) => Emitter | undefined;
+  _offByType: (type: TypeMain) => Emitter;
+  _offAll: () => Emitter;
+  events: {
+    [key: string]: Function[];
+  };
+  [key: string]: any;
+}
 
-function Emitter() {
+interface EmitterConstructor {
+  new (): Emitter;
+  [key: string]: any;
+}
+
+type TypeMain = string;
+type HandlerType = Function;
+type EventType = {
+  type: string;
+};
+type ArgsType = any[];
+
+interface CreateEvent {
+  new (type: EventType): EventType;
+  type: EventType;
+  timeStamp: Date;
+}
+
+const Emitter = (function (): Emitter {
   var e = Object.create(emitter);
   e.events = {};
   return e;
-}
+} as Function) as EmitterConstructor;
 
-function Event(type) {
+const CreateEvent = (function (this: CreateEvent, type: EventType) {
   this.type = type;
   this.timeStamp = new Date();
-}
+} as Function) as CreateEvent;
 
-emitter.on = function(type, handler) {
-  if (this.events.hasOwnProperty(type)) {
-    this.events[type].push(handler);
-  } else {
-    this.events[type] = [handler];
-  }
-  return this;
+var emitter: Emitter = {
+  events: {},
+  on: function (type, handler) {
+    if (this.events.hasOwnProperty(type)) {
+      this.events[type].push(handler);
+    } else {
+      this.events[type] = [handler];
+    }
+    return this;
+  },
+
+  off: function (type, handler) {
+    if (arguments.length === 0) {
+      return this._offAll();
+    }
+    if (handler === undefined) {
+      return this._offByType(type);
+    }
+    return this._offByHandler(type, handler);
+  },
+
+  trigger: function (event, args) {
+    if (!(event instanceof CreateEvent)) {
+      event = new CreateEvent(event);
+    }
+    return this._dispatch(event, args);
+  },
+  _dispatch: function (event, args) {
+    if (!this.events.hasOwnProperty(event.type)) return;
+    args = args || [];
+    args.unshift(event);
+
+    var handlers = this.events[event.type] || [];
+    handlers.forEach((handler) => handler.apply(null, args));
+    return this;
+  },
+
+  _offByHandler: function (type, handler) {
+    if (!this.events.hasOwnProperty(type)) return;
+    var i = this.events[type].indexOf(handler);
+    if (i > -1) {
+      this.events[type].splice(i, 1);
+    }
+    return this;
+  },
+
+  _offByType: function (type) {
+    if (this.events.hasOwnProperty(type)) {
+      delete this.events[type];
+    }
+    return this;
+  },
+  _offAll: function () {
+    this.events = {};
+    return this;
+  },
 };
 
-emitter.off = function(type, handler) {
-  if (arguments.length === 0) {
-    return this._offAll();
-  }
-  if (handler === undefined) {
-    return this._offByType(type);
-  }
-  return this._offByHandler(type, handler);
-};
+Emitter.Event = CreateEvent;
 
-emitter.trigger = function(event, args) {
-  if (!(event instanceof Event)) {
-    event = new Event(event);
-  }
-  return this._dispatch(event, args);
-};
-
-emitter._dispatch = function(event, args) {
-  if (!this.events.hasOwnProperty(event.type)) return;
-  args = args || [];
-  args.unshift(event);
-
-  var handlers = this.events[event.type] || [];
-  handlers.forEach(handler => handler.apply(null, args));
-  return this;
-};
-
-emitter._offByHandler = function(type, handler) {
-  if (!this.events.hasOwnProperty(type)) return;
-  var i = this.events[type].indexOf(handler);
-  if (i > -1) {
-    this.events[type].splice(i, 1);
-  }
-  return this;
-};
-
-emitter._offByType = function(type) {
-  if (this.events.hasOwnProperty(type)) {
-    delete this.events[type];
-  }
-  return this;
-};
-
-emitter._offAll = function() {
-  this.events = {};
-  return this;
-};
-
-Emitter.Event = Event;
-
-Emitter.mixin = function(obj, arr){
+Emitter.mixin = function (this: Emitter, obj: any, arr: any) {
   var emitter = new Emitter();
-  arr.map(function(name){
-    obj[name] = function(){
+  arr.map(function (name: string) {
+    obj[name] = function () {
       return emitter[name].apply(emitter, arguments);
     };
   });
