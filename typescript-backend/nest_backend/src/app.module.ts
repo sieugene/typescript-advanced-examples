ccf651a93d0b890385cb1593f2fca54eb746ab24
sieugene <sieugene@mail.ru>
2021-02-20 14:00:23 +0300

    generated

--- typescript-backend/nest_backend/src/app.module.ts
+++ typescript-backend/nest_backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 // Domains
 import { CatsModule } from './cats/cats.module';
+import { DogsModule } from './dogs/dogs.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://lab.lectrum.io:37019/cats'),
     CatsModule,
+    DogsModule,
   ],
 })
 export class AppModule {}
